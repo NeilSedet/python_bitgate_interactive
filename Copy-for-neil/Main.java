fbff3f9c52eec87f1beff890815a927bc98f8630
Neil Lin <neillin164@gmail.com>
2022-11-10 17:17:37 -0500

    what is this

--- Copy-for-neil/Main.java
+++
@@ -1,6 +0,0 @@
-//Date
-public class Main {
-  public static void main(String[] args) throws Exception {
-    GUI gui = new GUI();
-  }
-}
--- main.py
+++ main.py
@@ -2,23 +2,14 @@ import tkinter as tk
 from PIL import ImageTk, Image
 import time
 
-# make some sort of gui
-# get mouse input
-# get coordinates of that
-# get image displayed at that coordinate
 # process each image class and the inputs
 # do bitwise operations and display the final result
-
 # keep track of which image object connects to which one
 
-# make an array of locations
 # make each index an identifying number 'id'
-
-
 # make a method of 'selecting'
 # for each location, check if the mouse is within the bounds by using the resize parameters
 # then return the object id that it is selecting
-
 # make a menu to change functions
 
 window = tk.Tk()
@@ -26,35 +17,13 @@ window.title("TEST")
 window.geometry("700x900")
 # window.attributes('-fullscreen',True)
 
+image_obj = lambda: None
 
-#     img = Image.open(path).resize((40, 40))
 def imgCP(path):
+    global image_obj
+    img = Image.open(path).resize((40, 40))
     photo_image = ImageTk.PhotoImage(img)
     image_obj = lambda: tk.Label(window, image=photo_image)
-#     # return tk.Label(window, image=photo_image)
-
-#     return image_obj()
-
-# img = Image.open("./bin/gate_images/norGate.png").resize((40, 40))
-# photo_image = ImageTk.PhotoImage(img)
-# image_obj = lambda: tk.Label(window, image=photo_image)
-
-# clickButton = False
-# def orButton():
-#     global clickButton
-#     clickButton = True
-#     global image_obj
-#     img = Image.open("./bin/gate_images/norGate.png").resize((40, 40))
-#     photo_image = ImageTk.PhotoImage(img)
-#     image_obj = lambda: tk.Label(window, image=photo_image)
-
-
-# orButton = tk.Button(
-#     window,
-#     text="Red",
-#     command=orButton,
-# )
-# orButton.pack(side=tk.BOTTOM)
 
 
 class imgObjOR:
@@ -77,24 +46,25 @@ class imgObjOR:
 locations = []
 currPath = "./IMG.jpeg"
 
-
 def motion(event):
+    global image_obj, currPath, locations
     x, y = event.x, event.y
-    # global image_obj
 
-    img = imgCP(currPath)
-    img.place(x=x, y=y)
+    imgCP(currPath)
+    locations.append(image_obj())
+    locations[-1].place(x=x, y=y)
+    print(locations)
+    # new_obj.place(x=x, y=y)
 
     # try:
-    # image_obj.place(x=x, y=y)
     # image_obj().place(x=x, y=y)
-    new_obj = imgObjOR(x, y)
-    locations.append(new_obj)
+    # new_obj = imgObjOR(x, y)
+    # locations.append(new_obj)
 
     print("{}, {}".format(x, y))
     # except:
-
     #     print("Select an option dipshit")
+
 
 
 window.bind("<Button 1>", motion)
